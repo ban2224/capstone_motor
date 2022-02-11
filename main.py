072da53bffaaf88c784671cbb0a6ff5d52383d45
ban2224 <91554993+ban2224@users.noreply.github.com>
2022-02-11 11:29:44 -0500

    Update main.py
    added rohs4.py motor code in

--- main.py
+++ main.py
@@ -86,7 +86,73 @@ class Verify(QDialog):
         ea_li= self.ea_li.text()
         password_li = self.password_li.text()
         auth.sign_in_with_email_and_password(ea_li,password_li)
-        print("Successful verification")
+        
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+        aMotorPins = [8, 10, 11, 13]
+
+#IF time == true
+    #run motor
+# Set all pins as output
+        for pin in aMotorPins:
+          GPIO.setup(pin,GPIO.OUT)
+          GPIO.output(pin, False)
+        
+        aSequence = [
+            [1,0,0,1],
+            [1,0,0,0],
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,1,1,0],
+            [0,0,1,0],
+            [0,0,1,1],
+            [0,0,0,1]
+        ]
+        
+        iNumSteps = len(aSequence)
+
+##if sys.argv[3] == "cw":
+        iDirection = 1
+##else:
+##  iDirection = -1
+
+        fWaitTime = int(1) / float(1000)
+
+        iDeg = int(int(45) * 11.377777777777)
+
+        iSeqPos = 0
+# If the fourth argument is present, it means that the motor should start at a
+# specific position from the aSequence list
+        if len(sys.argv) > 4:
+            iSeqPos = int(sys.argv[4])
+
+# 1024 steps is 90 degrees
+# 4096 steps is 360 degrees
+
+        for step in range(0,iDeg):
+
+            for iPin in range(0, 4):
+                iRealPin = aMotorPins[iPin]
+                if aSequence[iSeqPos][iPin] != 0:
+                  GPIO.output(iRealPin, True)
+                else:
+                    GPIO.output(iRealPin, False)
+ 
+            iSeqPos += iDirection
+ 
+            if (iSeqPos >= iNumSteps):
+                iSeqPos = 0
+            if (iSeqPos < 0):
+                iSeqPos = iNumSteps + iDirection
+ 
+    # Time to wait between steps
+            time.sleep(fWaitTime)
+
+      for pin in aMotorPins:
+          GPIO.output(pin, False)
+        
+        
 
     def runSensor(self):
         ea_li = self.ea_li.text()
